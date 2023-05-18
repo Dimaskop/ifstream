bf51abf3e960ae8f4f1726cbcf3cfce9c94d60ce
Dmitriy Chebotarev <127520783+Dimaskop@users.noreply.github.com>
2023-05-18 17:51:29 +0300

    Добавьте файлы проекта.

---
+++ ifstream.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+   
+    int x;
+    ifstream fin  ("input.txt");
+    if (!fin.is_open()) {
+        x = -1;
+    }
+    else
+    {
+        fin >> x;
+    }
+        fin >> x;
+        cout << x;
+        fin.close();
+    system("pause>0");
+}
+
